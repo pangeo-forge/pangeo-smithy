2d944c397c3c5eac8aa0397dc23f832055af8df6
ryan.abernathey@gmail.com <ryan.abernathey@gmail.com>
2020-07-21 03:17:25 +0000

    add chunking

--- pipeline.py
+++ pipeline.py
@@ -4,7 +4,16 @@ import pandas as pd
 import os
 import fsspec
 import zarr
+import itertools
 
+# https://alexwlchan.net/2018/12/iterating-in-fixed-size-chunks/
+def chunked_iterable(iterable, size):
+    it = iter(iterable)
+    while True:
+        chunk = tuple(itertools.islice(it, size))
+        if not chunk:
+            break
+        yield chunk
 
 def src_url_from_key(key):
     source_url_pattern = 'https://www.ncei.noaa.gov/data/sea-surface-temperature-optimum-interpolation/v2.1/access/avhrr/{yyyymm}/oisst-avhrr-v02r01.{yyyymmdd}.nc'
@@ -33,22 +42,47 @@ def download(key):
 
 
 @task
-def combine_and_publish(sources, target):
+def combine_and_write(sources, target, append_dim, first=True):
     import xarray as xr
     # couldn't figure out how to do this with fsspec and open_mfdataset
     dsets = []
     double_open_files = [fsspec.open(url).open() for url in sources]
-    ds = xr.open_mfdataset(double_open_files, combine='nested', concat_dim='time')
-
-    mapper = fsspec.get_mapper(target)
-    ds.to_zarr(mapper)
-    zarr.consolidate_metadata(mapper)
+    ds = xr.open_mfdataset(double_open_files, combine='nested', concat_dim=concat_dim)
+ 
+    if first:
+        kwargs = dict(mode='w')
+    else:
+        kwargs = dict(mode='a', append_dim=append_dim)
     
-keys = pd.date_range('1981-09-01', '1981-09-06', freq='D')
+    mapper = fsspec.get_mapper(target)
+    ds.to_zarr(mapper, **kwargs)
+    
+@task
+def consolidate_metadata(target):
+    mapper = fsspec.get_mapper(target)
+    zarr.consolidate_metadata(mapper)
+
+
+keys = pd.date_range('1981-09-01', '1981-09-10', freq='D')
+target_path = 'gs://pangeo-scratch/rabernat/pangeo_smithy/oisst-avhrr-v02r01-target/'
+concat_dim = 'time'
+files_per_chunk = 5
 
 with Flow("Pangeo-Forge") as flow:
     sources = [download(k) for k in keys]
-    target_path = 'gs://pangeo-scratch/rabernat/pangeo_smithy/oisst-avhrr-v02r01-target/'
-    combine_and_publish(sources, target_path)
-    
+    first = True
+    write_tasks = []
+    for source_group in chunked_iterable(sources, files_per_chunk):
+        write_task = combine_and_write(source_group, target_path, concat_dim, first=first)
+        write_tasks.append(write_task)
+        first = False
+    cm = consolidate_metadata(target_path)
+
+# create dependencies in imperative mode
+for n in range(1, len(write_tasks)):
+    write_tasks[n].set_upstream(write_tasks[n-1], flow=flow)
+cm.set_upstream(write_tasks[-1], flow=flow)
+
+
+### DO IT! ###
 flow.run()
